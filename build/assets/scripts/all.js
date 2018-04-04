d24859dae5463e452eb3945ff7c0e3e1b64cae1a
Aleksandr <ashvalev78@gmail.com>
2018-04-04 19:20:35 +0300

    map included

--- build/assets/scripts/all.js
+++ build/assets/scripts/all.js
@@ -40,6 +40,76 @@ function flip() {
 }
 'use strict';
 
+function initMap() {
+  // Styles a map in night mode.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 59.846688, lng: 30.229678 },
+    zoom: 12,
+    styles: [{ elementType: 'geometry', stylers: [{ color: '#242f3e' }] }, { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] }, { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] }, {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#d59563' }]
+    }, {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#d59563' }]
+    }, {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{ color: '#263c3f' }]
+    }, {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#6b9a76' }]
+    }, {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{ color: '#FFFFFF' }]
+    }, {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#FFFFFF' }]
+    }, {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#FFFFFF' }]
+    }, {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{ color: 'grey' }]
+    }, {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: 'grey' }]
+    }, {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: 'grey' }]
+    }, {
+      featureType: 'transit',
+      elementType: 'geometry',
+      stylers: [{ color: 'grey' }]
+    }, {
+      featureType: 'transit.station',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: 'grey' }]
+    }, {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{ color: '#61dac9' }]
+    }, {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#61dac9' }]
+    }, {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{ color: '#61dac9' }]
+    }]
+  });
+}
+'use strict';
+
 function parallax() {
 
     var parallaxSection = document.getElementById('parallax-section');
