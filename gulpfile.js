37caad5d79a76b0ee548a19b7154ae3e76098ab0
Aleksandr <ashvalev78@gmail.com>
2018-04-06 14:35:11 +0300

    some shit

--- gulpfile.js
+++ gulpfile.js
@@ -1,176 +1,178 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
+
+// плагины галпа, объявлять не нужно, используем как $gp.имяПлагина (без приставки gulp-)
 const $gp = require("gulp-load-plugins")();
-const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const gulpWebpack = require('gulp-webpack');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
-const babel = require("gulp-babel");
-const concat = require("gulp-concat");
-const eslint = require("eslint");
-var svgSprite = require("gulp-svg-sprite");
 
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config.js");
+const moduleImporter = require("sass-module-importer");
+const del = require("del");
+const pug = require("gulp-pug");
 
-// paths to the files destination
-const paths = {
-    root: './build',
-    templates: {
-        pages: 'src/templates/pages/*.pug',
-        src: 'src/templates/**/*.pug'
-    },
-    styles: {
-        src: 'src/styles/**/*.scss',
-        dest: 'build/assets/styles/'
-    },
-    images: {
-        src: 'src/images/**/*.*',
-        dest: 'build/assets/images/',
-        svg: 'src/images/icons/*.svg',
-        svgDest: 'build/assets/images/icons/'
-    },
-    scripts: {
-        src: 'src/scripts/**/*.js',
-        dest: 'build/assets/scripts/'
-    }
-};
+const SRC_DIR = "src";
+const DIST_DIR = "build";
+const ROOT_PATH = `./${DIST_DIR}`;
 
-// SVG sprites
-gulp.task("svg", done => {
-    const prettySvgs = () => {
-        return gulp
-            .src(paths.images.svg)
-            .pipe(
-                $gp.svgmin({
-                    js2svg: {
-                        pretty: true
-                    }
-                })
-            )
-            .pipe(
-                $gp.cheerio({
-                    run($) {
-                        $("[fill], [stroke], [style]")
-                            .removeAttr("fill")
-                            .removeAttr("stroke")
-                            .removeAttr("style");
-                    },
-                    parserOptions: {xmlMode: true}
-                })
-            )
-            .pipe($gp.replace("&gt;", ">"));
-    };
-
-    prettySvgs()
-        .pipe(
-            $gp.svgSprite({
-                mode: {
-                    symbol: {
-                        sprite: "../sprite.svg"
-                    }
-                }
-            })
-        )
-        .pipe(gulp.dest(paths.images.svgDest));
-
-    prettySvgs().pipe(
-        $gp.sassInlineSvg({
-            destDir: "src/styles/icons/"
-        })
-    );
-
-    done();
+// стили
+gulp.task("styles", () => {
+  return gulp
+    .src(`${SRC_DIR}/styles/app.scss`)
+    .pipe($gp.plumber())
+    .pipe($gp.sassGlob())
+    .pipe($gp.sourcemaps.init())
+    .pipe(
+      $gp.sass({
+        outputStyle: "compressed",
+        importer: moduleImporter()
+      })
+    )
+    .pipe(
+      $gp.autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe($gp.sourcemaps.write())
+    .pipe($gp.rename({ suffix: ".min" }))
+    .pipe(gulp.dest(`${DIST_DIR}/styles/`))
+    .pipe(reload({ stream: true }));
 });
 
+// переносим шрифты
+gulp.task("fonts", () => {
+  return gulp.src(`${SRC_DIR}/fonts/**`).pipe(gulp.dest(`${DIST_DIR}/fonts/`));
+});
 
-// babel
-function useBabel() {
-    return gulp.src("src/**/*.js")
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat("all.js"))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest(paths.scripts.dest));
-}
+// очистка
+gulp.task("clean", () => {
+  return del(ROOT_PATH);
+});
 
-// linting js
-function lint() {
-//     return gulp.src('src/**/*.js')
-//         .pipe(eslint())
-//         .pipe(eslint.format())
-//         .pipe(eslint.failAfterError());
-}
+// собираем скрипты webpack
+gulp.task("scripts", () => {
+  return gulp
+    .src(`${SRC_DIR}/scripts/app.js`)
+    .pipe($gp.plumber())
+    .pipe($gp.webpack(webpackConfig, webpack))
+    .pipe(gulp.dest(`${DIST_DIR}/scripts`))
+    .pipe(reload({ stream: true }));
+});
+
+// сервер node.js
+gulp.task("nodemon", done => {
+  let started = false;
+  $gp
+    .nodemon({
+      script: "server.js",
+      env: { NODE_ENV: "development" },
+      watch: "server.js"
+    })
+    .on("start", () => {
+      if (started) return;
+      done();
+      started = true;
+    });
+});
+
+// dev сервер + livereload (встроенный)
+gulp.task(
+  "server",
+  gulp.series("nodemon", done => {
+    browserSync.init({
+      proxy: "http://localhost:3000",
+      port: 8080,
+      open: true
+    });
+  })
+);
+
+// спрайт иконок + инлайн svg
+gulp.task("svg", done => {
+  const prettySvgs = () => {
+    return gulp
+      .src(`${SRC_DIR}/images/icons/*.svg`)
+      .pipe(
+        $gp.svgmin({
+          js2svg: {
+            pretty: true
+          }
+        })
+      )
+      .pipe(
+        $gp.cheerio({
+          run($) {
+            $("[fill], [stroke], [style], [width], [height]")
+              .removeAttr("fill")
+              .removeAttr("stroke")
+              .removeAttr("style")
+              .removeAttr("width")
+              .removeAttr("height");
+          },
+          parserOptions: { xmlMode: true }
+        })
+      )
+      .pipe($gp.replace("&gt;", ">"));
+  };
+
+  prettySvgs()
+    .pipe(
+      $gp.svgSprite({
+        mode: {
+          symbol: {
+            sprite: "../sprite.svg"
+          }
+        }
+      })
+    )
+    .pipe(gulp.dest(`${DIST_DIR}/images/icons`));
+
+  prettySvgs().pipe(
+    $gp.sassInlineSvg({
+      destDir: `${SRC_DIR}/styles/icons/`
+    })
+  );
+
+  done();
+});
 
 // pug
-function templates() {
-    return gulp.src(paths.templates.pages)
-        .pipe(pug({pretty: true}))
-        .pipe(gulp.dest(paths.root));
-}
+gulp.task( 'templates', () => {
+  return gulp.src(`${SRC_DIR}/templates/pages/*.pug`)
+      // .pipe(pug({pretty: true}))
+      .pipe(gulp.dest(`${DIST_DIR}`));
+});
 
-// scss - compression and renaming
-function styles() {
-    return gulp.src('./src/styles/app.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(sourcemaps.write())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(paths.styles.dest))
-}
+// просто переносим картинки
+gulp.task("images", () => {
+  return gulp
+    .src([`${SRC_DIR}/images/**/*.*`, `!${SRC_DIR}/images/icons/*.*`])
+    .pipe(gulp.dest(`${DIST_DIR}/images/`));
+});
 
-// deleting
-function clean() {
-    return del(paths.root);
-}
-
-// webpack
-function scripts() {
-    return gulp.src('src/scripts/*.js')
-        .pipe(gulpWebpack(webpackConfig, webpack))
-        .pipe(gulp.dest(paths.scripts.dest));
-}
-
-// livereload
-function server() {
-    browserSync.init({
-        server: paths.root
-    });
-    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
-}
-
-// images transfer
-function images() {
-    return gulp.src(paths.images.src)
-        .pipe(gulp.dest(paths.images.dest));
-}
-
-function fonts() {
-  return gulp.src('src/fonts/**/*.*')
-    .pipe(gulp.dest('build/assets/fonts'));
-}
-
-exports.templates = templates;
-exports.styles = styles;
-exports.clean = clean;
-exports.images = images;
-
-
-// watcher
-function watch() {
-    gulp.watch(paths.styles.src, styles);
-    gulp.watch(paths.templates.src, templates);
-    gulp.watch(paths.images.src, images);
-    gulp.watch(paths.scripts.src, scripts);
-    gulp.watch(paths.scripts.src, useBabel);
-    gulp.watch(paths.scripts.src, lint);
-}
-
-// default task
-gulp.task('default', gulp.series(
-    clean,
-    gulp.parallel("svg", templates, images, scripts, styles, fonts),
-    gulp.parallel(watch, server)
+gulp.task('build', gulp.series(
+  "svg",
+  gulp.parallel("styles", "images", "fonts", "scripts")
 ));
+
+// галповский вотчер
+gulp.task("watch", () => {
+  gulp.watch(`${SRC_DIR}/styles/**/*.scss`, gulp.series("styles"));
+  gulp.watch(`${SRC_DIR}/images/**/*.*`, gulp.series("images"));
+  gulp.watch(`${SRC_DIR}/scripts/**/*.js`, gulp.series("scripts"));
+  gulp.watch(`${SRC_DIR}/fonts/*`, gulp.series("fonts"));
+  gulp.watch(`${SRC_DIR}/templates/**/*.*`, gulp.series("templates"));
+  gulp.watch(`${DIST_DIR}`).on('change', reload);
+  gulp.watch()
+});
+
+// GULP:RUN
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    gulp.parallel("styles", "images", "fonts", "scripts", "svg", "templates"),
+    gulp.parallel("watch", "server")
+  )
+);
