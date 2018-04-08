fbdaa6a5f329f61013c07b30084efda30047640c
Aleksandr <ashvalev78@gmail.com>
2018-04-08 17:50:45 +0300

    blur added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,14 +2,26 @@ import flip from './common/flipper'
 import menu from './common/fullMenu'
 import initMap from './common/map'
 import parallax from './common/parallax'
+import blur from './common/blur'
 
 
 
 function start() {
-    parallax();
-    flip();
-    initMap();
-    menu();
+    const s = location.href;
+    const name = s.substr(s.lastIndexOf("/")+1);
+    if (name !== "index.html") {
+        menu();
+    }
+    if (name === "index.html") {
+        parallax();
+        flip();
+    }
+    if (name === "about.html") {
+        initMap();
+    }
+    if (name === "my-works.html") {
+        blur();
+    }
 }
 
 window.onload = start;
